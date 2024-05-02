342d4641fc7a78f187da247ee11c3374655496c9
FelipeSchreiber <schreiber.felipe@poli.ufrj.br>
2024-05-02 14:26:38 -0300

    ignore poly

--- stop_all.sh
+++ stop_all.sh
@@ -1,2 +1,3 @@
 #! /bin/bash
 docker ps -a --filter ancestor='overv/openstreetmap-tile-server' --format="{{.ID}}" | xargs docker stop 
+#docker ps -a --filter ancestor='overv/openstreetmap-tile-server' --format="{{.ID}}" | xargs docker stop | xargs docker rm
