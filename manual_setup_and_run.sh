71542d3499324f24285de973b8301134dd16f171
FelipeSchreiber <schreiber.felipe@poli.ufrj.br>
2024-05-02 15:20:23 -0300

    Create volume if doesnt exist

--- manual_setup_and_run.sh
+++ manual_setup_and_run.sh
@@ -1,7 +1,10 @@
 #! /bin/bash
 region=south-america/brazil
 port="7070:70"
-docker volume create osm-data
+vol=$(docker volume inspect osm-data)
+if [[ $?=1 ]] then;
+    docker volume create osm-data
+fi
 if ! [ -f *osm.pbf ]; then
     ./download_data.sh -r "$region"
 fi
