ad62a6fd546a1ea826d19b512e1201ab55a3b365
FelipeSchreiber <schreiber.felipe@poli.ufrj.br>
2024-05-16 17:46:11 -0300

    added container prune in stop_all.sh

--- stop_all.sh
+++ stop_all.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
-docker ps -a --filter ancestor='overv/openstreetmap-tile-server' --format="{{.ID}}" | xargs docker stop | xargs docker rm
+docker ps -a --filter ancestor='overv/openstreetmap-tile-server' --format="{{.ID}}" | xargs docker stop | xargs docker image rm
+docker container prune -f
 docker volume rm osm-data
-#docker ps -a --filter ancestor='overv/openstreetmap-tile-server' --format="{{.ID}}" | xargs docker stop | xargs docker rm
